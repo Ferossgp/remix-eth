8a6f06ef1ae54c8f9b51bc6bb78c1de1cdfe3790
Gheorghe Pinzaru <feross95@gmail.com>
2024-02-16 20:39:08 +0100

    Setup turbo repo with remix and hardhat

---
+++ apps/web/app/routes/_index.tsx
@@ -0,0 +1,36 @@
+import type { MetaFunction } from "@remix-run/node";
+import { Button } from "~/components/ui/button";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix SPA" },
+    { name: "description", content: "Welcome to Remix (SPA Mode)!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1>Welcome to Remix (SPA Mode)</h1>
+      <ul>
+        <li>
+          <Button asChild>
+            <a
+              target="_blank"
+              href="https://remix.run/future/spa-mode"
+              rel="noreferrer"
+            >
+              SPA Mode Guide
+            </a>
+          </Button>
+
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
